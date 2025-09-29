4cd38e884c2c246f19e9df8e85100d3e0e02c654
VELSIMIT <velsimit@gmail.com>
2025-09-29 19:57:35 +0200

    Iprog HA

---
+++ IPROG‑LAB‑01/Course.java
@@ -0,0 +1,43 @@
+/**
+@vitalii.vlasiuk
+*/
+
+public class Course {
+    Student s1;
+    Student s2;
+    Student s3;
+    Student s4;
+    Student s5;
+    Student s6;
+    Student s7;
+    Student s8;
+    Student s9;
+    Student s10;
+
+    public void addStudents(Student student1,
+                            Student student2, 
+                            Student student3,
+                            Student student4,
+                            Student student5,
+                            Student student6,
+                            Student student7,
+                            Student student8,
+                            Student student9,
+                            Student student10){
+    this.s1 = student1;
+    this.s2 = student2;
+    this.s3 = student3;
+    this.s4 = student4;
+    this.s5 = student5;
+    this.s6 = student6;
+    this.s7 = student7;
+    this.s8 = student8;
+    this.s9 = student9;
+    this.s10 = student10;
+    }
+
+    String name;
+    public String getName(){
+        return this.name;
+    } 
+}
---
+++ IPROG‑LAB‑01/Student.java
@@ -0,0 +1,71 @@
+/**
+@vitalii.vlasiuk
+*/
+
+public class Student {
+    private String name;
+    private String firstName;
+    private int studentId;
+    private int age;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+//....................................................................................
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//....................................................................................
+
+    public String getName() {
+    return this.name;
+    }
+
+    public String getFirstName() {
+    return this.firstName;
+    }
+
+    public int getStudentId() {
+    return this.studentId;
+    }
+
+    public int getAge() {
+    return this.age;
+    }
+
+    public String getAddress() {
+    return this.address;
+    }
+
+    public String getPhoneNumber() {
+    return this.phoneNumber;
+    }
+
+    public String getEmail() {
+    return this.email;
+    }
+
+//....................................................................................
+
+    public Student (String name, String firstName, int studentId) {
+        this.name = name;
+        this.firstName = firstName;
+        this.studentId = studentId;
+    }
+}
+
+
