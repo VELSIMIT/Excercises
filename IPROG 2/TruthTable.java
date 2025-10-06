220c0ce51482a024a6690a341f0ee05cff88bd0f
VELSIMIT <velsimit@gmail.com>
2025-10-06 11:31:39 +0200

    abgabe

---
+++ IPROG 2/TruthTable.java
@@ -0,0 +1,31 @@
+/**
+@vitalii.vlasiuk
+*/
+public class TruthTable {
+
+  public static void main(String [] args){
+
+    boolean a = true;
+    boolean b = true;
+
+    System.out.println("|\tA\t|\tB\t|\tNOT A\t|\tA AND B\t|\tA OR B\t|\tA XOR B\t|");
+    System.out.println("=================================================================================================");
+    System.out.println("|\t" + a + "\t|\t" + b + "\t|\t" + !a + "\t|\t" + (a & b) + "\t|\t" + (a | b) + "\t|\t" + (a ^ b) + "\t|");
+    
+    a = true;
+    b = false;
+
+    System.out.println("|\t" + a + "\t|\t" + b + "\t|\t" + !a + "\t|\t" + (a & b) + "\t|\t" + (a | b) + "\t|\t" + (a ^ b) + "\t|");
+
+    a = false;
+    b = true;
+
+    System.out.println("|\t" + a + "\t|\t" + b + "\t|\t" + !a + "\t|\t" + (a & b) + "\t|\t" + (a | b) + "\t|\t" + (a ^ b) + "\t|");
+
+    a = false;
+    b = false;
+
+    System.out.println("|\t" + a + "\t|\t" + b + "\t|\t" + !a + "\t|\t" + (a & b) + "\t|\t" + (a | b) + "\t|\t" + (a ^ b) + "\t|");
+
+  }
+}
