935e423410ccc166c00e9abfc94b99b54a942df7
VELSIMIT <velsimit@gmail.com>
2025-10-07 12:47:28 +0200

    successfully created a loop with 2d matrix

--- IPROG 2/Loopex.java
+++ IPROG 2/Loopex.java
@@ -4,13 +4,41 @@
 
 public class Loopex {
   public static void main(String [] args){
-    String[][] matrix = new String [5][5];
-    int rowscounter = 0;
+    int linescounter = 0;
+    int colums = 6;
+    int a = 0;
 
-
-    while (rowscounter <=6){
-
+    while(linescounter <= 5){
       
+      if(linescounter == 0){
+        while (a < colums){
+          System.out.print("*");
+          a++;
+        }
+        linescounter++;
+        a = 0;
+        System.out.println();
+      }
+      
+      else if(linescounter > 0){
+
+        while(a < linescounter) {
+          System.out.print(".");
+          a++;
+        }
+
+        while (a < colums) {
+          System.out.print("*");
+          a++;
+        }
+
+        if (a <= 6) {
+          System.out.println();
+        }
+        
+        a = 0;
+        linescounter++;
+      }
     }
-   }
+  }
 }
