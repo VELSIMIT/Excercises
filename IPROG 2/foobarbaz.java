3e4ce1344806ca2fee17681f5a4efbd551db92e8
VELSIMIT <velsimit@gmail.com>
2025-10-09 14:12:55 +0200

    updated to the task

--- IPROG 2/foobarbaz.java
+++ IPROG 2/foobarbaz.java
@@ -3,10 +3,10 @@
  */
 public class Foobarbaz {
   public static void main(String[] args) {
+    
     int i = 0;
-
     while( i < args.length) {
-      System.out.print(args[i] + " ");
+      System.out.println(args[i]);
       i++;
     }
   }
