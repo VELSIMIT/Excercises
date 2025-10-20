cdb94847e9e17b4dce80f3024770244dbed1904a
VELSIMIT <velsimit@gmail.com>
2025-10-20 12:56:40 +0200

    Refactor LoopTest class to streamline argument handling and improve readability

--- IPROG 3/LoopTest.java
+++ IPROG 3/LoopTest.java
@@ -3,6 +3,7 @@
  */
 public class LoopTest {
   public static void main(String[] args) {
+    int parameterInt;
     if (args.length == 0) {
       System.out.println("No arguments");
       return;
@@ -13,35 +14,29 @@ public class LoopTest {
       return;
     }
 
-    int parameterInt;
-   
     try {
       parameterInt = Integer.parseInt(args[1]);
     } catch (NumberFormatException e) {
         System.out.println("Invalid arguments");
         return;
-    }
+      }
 
     if (args[0].equals("-d")) {
-      int y = parameterInt;
       do {
-        System.out.println(y);
-        y--;
-        } while (y > 0);
+        System.out.println(parameterInt);
+        parameterInt--;
+      } while (parameterInt > 0);
       return;
     }
 
     if (args[0].equals("-w")) {
-      int x = parameterInt;
-      while (x > 0) {
-        System.out.println(x);
-        x--;
-        }
+      while (parameterInt > 0) {
+        System.out.println(parameterInt);
+        parameterInt--;
+      }
       return;
     }
-
     System.out.println("Invalid arguments");   
-
   }
 }
 
